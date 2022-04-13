13d0d5b2e7fae91d4716bb5066355a2556640559
marloestacx <marloes.tacx@live.nl>
2022-04-13 11:05:37 +0200

    Update: store color local

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,22 +1,37 @@
+// get values when loading
+window.onload = () => {
+  getValues();
+};
+
+// fill input fields
+const getValues = () => {
+  const body = document.querySelector("body");
+  const item = localStorage.getItem("color");
+  console.log(item);
+
+  body.classList.add(item);
+};
+
 function menuOpen() {
-    const menu_btn = document.querySelector("#chooseBG");
-    const color_list = document.querySelector("#colorArray");
-    
-    menu_btn.addEventListener("click", function() {
-        color_list.classList.toggle("hidden");
-    });
+  const menu_btn = document.querySelector("#chooseBG");
+  const color_list = document.querySelector("#colorArray");
+
+  menu_btn.addEventListener("click", function () {
+    color_list.classList.toggle("hidden");
+  });
 }
 menuOpen();
 
 function colorPicker() {
-    const color_btns = document.querySelectorAll("#colorArray button");
-    const body = document.querySelector("body");
+  const color_btns = document.querySelectorAll("#colorArray button");
+  const body = document.querySelector("body");
 
-    color_btns.forEach(function (element) {
-        element.addEventListener("click", function() {
-            body.className = "";
-            body.classList.add(this.value);
-        });
+  color_btns.forEach(function (element) {
+    element.addEventListener("click", function () {
+      body.className = "";
+      body.classList.add(this.value);
+      localStorage.setItem("color", this.value);
     });
+  });
 }
 colorPicker();
